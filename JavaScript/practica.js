0d2992f5403a64e25fd64c870920e41895cd619c
Leonardo Lagos <leonardolagos07@hotmail.com>
2022-05-30 18:04:50 -0300

    ajustes

--- JavaScript/practica.js
+++ JavaScript/practica.js
@@ -1,8 +1,22 @@
+alert("Debes ingresar un valor numerico o escribir 'ESC' para salir");
 let numA = prompt("ingresar un numero");
 let numB = prompt("Ingrese otro numero");
 while(numA != "ESC"){
-    let suma = Number(numA) + Number(numB);
-    alert(suma);
-    numB=suma;
-    numA=prompt("ingresa otro numero");
-}
+    if((isNaN(numA))||(isNaN(numB))){
+        alert("Debes ingresar solo numeros, recarga la pagina para continuar");
+        break;
+    }
+    else if((numA=="ESC")||(numB=="ESC")){
+        break;
+    }
+    else{
+        let suma = Number(numA) + Number(numB);
+        alert(suma);
+        numB=suma;
+        numA=prompt("ingresa otro numero");
+        if(isNaN(numA)){
+            alert("Debes ingresar solo numeros, recarga la pagina para continuar");
+            break;
+        }
+    }
+}                
