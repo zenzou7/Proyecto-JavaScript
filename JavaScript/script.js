cc906f97cc72db1d6ab03bfeb07ec35e3daf0b0e
Leonardo Lagos <leonardolagos07@hotmail.com>
2022-06-30 19:07:53 -0300

    trabajo con JSON y localStorage

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,19 +7,26 @@ let mostrarProductosAll = document.getElementById("mostrarProductosAll");
 function mostrarProductos(){
     bebidas.forEach((bebida)=>{
         let card = document.createElement("div")
+        card.setAttribute("class", "card")
         card.innerHTML += `
-        <img src="${bebida.img}">
-        <h3>${bebida.nombre}</h3>
-        <p>$${bebida.precio}</p>`
+        <img class="card__img" src="${bebida.img}">
+        <h3 class="card__nombre">${bebida.nombre}</h3>
+        <p class="card__precio">$${bebida.precio}</p>`
         let botonCompra = document.createElement("button")
         botonCompra.innerText = ("Agregar al carrito")
         card.append(botonCompra)
         mostrarProductosAll.append(card)
 
         botonCompra.addEventListener("click", function(){
-            carrito.push(bebida)
-            divCarrito.innerHTML=``
-             mostrarCarrito(); 
+            if(carrito.filter(bebida => bebida.cantidad >=1) == bebida){
+                bebida.cantidad= bebida.cantidad + 1;
+            }
+            else{
+                carrito.push(bebida)
+                bebida.cantidad= bebida.cantidad + 1;
+                divCarrito.innerHTML=``
+                mostrarCarrito(); 
+            }
         })
     })
 }
@@ -35,33 +42,34 @@ let divCarrito = document.getElementById("divCarrito")
 let carritoAlert = document.createElement("h2")
 carritoAlert.setAttribute("class", "alerta")
 
-if(carrito.length==0){
+if(carrito.length = 0){
     carritoAlert.innerText = ("Aun no tienes productos en tu carro")
     divCarrito.append(carritoAlert)
 }
 
-
+else{
 function mostrarCarrito(){
     carritoAlert.remove()
 
+    //Elementos del carrito
     carrito.forEach((bebida) => {
         const carritoDiv = document.createElement("li")
         carritoDiv.innerHTML += `
-        <img src="${bebida.img}">
-        <h3>${bebida.nombre}</h3>
-        <h3>$${bebida.precio}</h3>`
+        <img class="card__img" src="${bebida.img}">
+        <h3 class="card__nombre">${bebida.nombre}</h3>
+        <h3 class="card__precio">$${bebida.precio}</h3>`
         let botonBorrar = document.createElement("button")
         botonBorrar.innerText = ("X")
         botonBorrar.setAttribute("data",bebida.nombre)
         carritoDiv.append(botonBorrar)
         divCarrito.append(carritoDiv)
-        
-        
+            
         botonBorrar.onclick = (e) =>{
             const bebidaNombre = e.target.getAttribute(`data`)
             carrito = carrito.filter(bebida => bebida.nombre !== bebidaNombre)
             divCarrito.innerHTML=``
-            mostrarCarrito()
+
+            mostrarCarrito();
         }
     })
 
@@ -78,4 +86,38 @@ function mostrarCarrito(){
     costoTotal.innerText =(`Costo total: $${total()}`)
     divCarrito.append(costoTotal)
 }
+function mostrarBotones(){
+    
+        //Botones del storage
+        const botonGuardar = document.createElement("button")
+        botonGuardar.innerText = ("Guardar Carrito")
+        botonGuardar.setAttribute("id", "botonGuardar")
+        carritoLista.append(botonGuardar)
+
+        botonGuardar.onclick = function guardarLS(){
+            localStorage.setItem("carrito", JSON.stringify(carrito))
+        }
+
+        const botonBorrar = document.createElement("button")
+        botonBorrar.innerText = ("Borrar Carrito")
+        botonBorrar.setAttribute("id", "botonBorrar")
+        carritoLista.append(botonBorrar)
+        botonBorrar.addEventListener("click", function(){
+            localStorage.clear()
+        })
+}
+mostrarBotones()
+}
+//verifico si existe el local storage
+
+if(localStorage){
+const bebidasLS = JSON.parse(localStorage.getItem("carrito"));
+
+
+for (const objeto of bebidasLS){
+    carrito.push(new BebidaLS(objeto));
+}
+mostrarCarrito()
+}
+
 
--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -11,9 +11,20 @@ class Bebida{
 }
 
 const bebida1 = new Bebida("./img/fernet_branca.jpg","Fernet Branca", 800, 0, 0);
-const bebida2 = new Bebida("./img/monster.jpg","Monster", 300, 0, 0);
+const bebida2 = new Bebida("./img/monster.png","Monster", 300, 0, 0);
 const bebida3 = new Bebida("./img/vodka_absolut.jpg","Vodka Absolut", 1000, 0, 0);
 
 let bebidas= [bebida1,bebida2,bebida3];
+
+class BebidaLS {
+    constructor(obj) {
+        this.img = obj.img
+        this.nombre  = obj.nombre
+        this.precio  = parseFloat(obj.precio);
+        this.cantidad = parseFloat(obj.cantidad);
+        this.total = parseFloat(obj.total);
+    }
+}
+
 
  
